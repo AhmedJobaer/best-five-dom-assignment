df01a8a379e8ff096c0155ecab113d44ae3c63dd
jobaer-ahmed <ajobaer08@gmail.com>
2022-08-22 15:14:52 +0800

    cart added

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+
+
+let count = 0;
+function addToCart(element) {
+    if (count < 5) {
+        const playerName = element.parentNode.children[0].innerText;
+        count++;
+        const cart = document.getElementById('cart-list');
+        const list = document.createElement('li');
+        list.innerText = count + '. ' + playerName;
+        list.style.marginBottom = '10px';
+        cart.appendChild(list)
+        document.getElementById('btn-select').style.backgroundColor = 'gray';
+    }
+    else {
+        alert("You can not added more than 5 player!")
+    }
+
+}
+
