6a15560df7767a73adfff163455814d8d552dcc4
jobaer-ahmed <ajobaer08@gmail.com>
2022-08-22 15:47:55 +0800

    cart done with disable button

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 
 let count = 0;
 function addToCart(element) {
+
     if (count < 5) {
         const playerName = element.parentNode.children[0].innerText;
         count++;
@@ -10,12 +11,13 @@ function addToCart(element) {
         const list = document.createElement('li');
         list.innerText = count + '. ' + playerName;
         list.style.marginBottom = '10px';
-        cart.appendChild(list)
-        document.getElementById('btn-select').style.backgroundColor = 'gray';
+        cart.appendChild(list);
     }
     else {
         alert("You can not added more than 5 player!")
     }
+
+    return count;
 
 }
 
