e69a893053597f0397b728ec15557963333fff07
jobaer-ahmed <ajobaer08@gmail.com>
2022-08-22 17:47:53 +0800

    per player budget calulation done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 
-
-
 let count = 0;
 function addToCart(element) {
 
@@ -12,12 +10,38 @@ function addToCart(element) {
         list.innerText = count + '. ' + playerName;
         list.style.marginBottom = '10px';
         cart.appendChild(list);
+        const playerList = document.getElementById('player-list');
+        playerList.innerText = count;
     }
     else {
-        alert("You can not added more than 5 player!")
+        alert("You already added 5 player. Can not added more than 5 player!")
     }
 
-    return count;
+
 
 }
 
+function getInputById(elementId) {
+    const inputElement = document.getElementById(elementId);
+    const inputElementString = inputElement.value;
+    const inputValue = parseFloat(inputElementString);
+    return inputValue;
+}
+
+function getElementInputById(elementId) {
+    const elementInput = document.getElementById(elementId);
+    const elementInputString = elementInput.innerText;
+    const elementValue = parseFloat(elementInputString);
+    return elementValue;
+}
+
+document.getElementById('players-cost-btn').addEventListener('click', function () {
+    const perPlayeCost = getInputById('per-player-cost');
+    const playerTotal = getElementInputById('player-list')
+    const totalPlayerCost = perPlayeCost * playerTotal;
+
+    const playerExpences = document.getElementById('playerExpences');
+    playerExpences.innerText = totalPlayerCost;
+
+    console.log(totalPlayerCost);
+})
